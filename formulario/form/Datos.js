b50ec1d9a6d0fae55b0ede7553054ad5cbb65327
Ivan <ivanalejandro1996@gmail.com>
2018-04-26 00:19:34 -0430

    Hacer validaciones

--- formulario/form/Datos.js
+++ formulario/form/Datos.js
@@ -1,6 +1,7 @@
 var app = angular.module('Formulario', []);
 app.controller('Controlador', ['$scope', function($scope){
     $scope.usuario={
-     
+      contraseña:"",
+      confirmar:""
     };
 }]);
